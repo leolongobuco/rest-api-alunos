21f5ec15d2f2251c31a49fce4b7dd63c43046308
Leonardo Longobuco <longobucoleonardo@gmail.com>
2021-10-02 20:07:16 -0300

    CRUD users e correção de bug hash user durante update

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,9 +4,92 @@ class UserController {
   async store(req, res) {
     try {
       const user = await User.create(req.body);
-      res.json(user);
+      return res.status(201).json(user);
     } catch (error) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: error.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.status(200).json(users);
+    } catch (error) {
+      return res.status(404).json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { userId } = req.params;
+      if (!userId) {
+        return res.status(400).json({
+          errors: ["Missing ID"],
+        });
+      }
+      const user = await User.findByPk(userId);
+      if (!user) {
+        return res.status(400).json({
+          errors: ["User doesn't exist"],
+        });
+      }
+      return res.status(200).json(user);
+    } catch (error) {
+      return res.status(400).json("User not found");
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { userId } = req.params;
+
+      if (!userId) {
+        return res.status(400).json({
+          errors: ["Missing ID"],
+        });
+      }
+
+      const user = await User.findByPk(userId);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ["User doesn't exist"],
+        });
+      }
+
+      const newUser = await user.update(req.body);
+      return res.status(200).json(newUser);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { userId } = req.params;
+
+      if (!userId) {
+        return res.status(400).json({
+          errors: ["Missing ID"],
+        });
+      }
+
+      const user = await User.findByPk(userId);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ["User doesn't exist"],
+        });
+      }
+
+      await user.destroy();
+      return res.status(204);
+    } catch (error) {
+      return res.status(400).json({
         errors: error.errors.map((error) => error.message),
       });
     }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,5 +8,9 @@ router.use(logRequests);
 
 //Rotas Users
 router.post("/", userController.store);
+router.get("/", userController.index);
+router.get("/:userId", userController.show);
+router.put("/:userId", userController.update);
+router.delete("/:userId", userController.delete);
 
 export default router;
