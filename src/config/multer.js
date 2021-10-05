149ca8b53998859c2df6589df42b73def44c9d9b
Leonardo Longobuco <longobucoleonardo@gmail.com>
2021-10-04 21:05:28 -0300

    validação do tipo de arquivo enviado pelo cliente e melhora na org dos arquivos

--- src/config/multer.js
+++ src/config/multer.js
@@ -3,7 +3,19 @@ import { extname, resolve } from "path";
 
 const random = () => Math.floor(Math.random() * 10000 + 10000);
 
+const isImage = (typeFile) => {
+  if (typeFile === "image/png" || typeFile === "image/jpeg") return true;
+  return false;
+};
+
 export default {
+  fileFilter: (req, file, callback) => {
+    if (!isImage(file.mimetype)) {
+      return callback(new multer.MulterError("Arquivo precisa ser PNG ou JPG"));
+    }
+
+    return callback(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, callback) => {
       callback(null, resolve(__dirname, "..", "..", "uploads"));
--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -1,16 +1,12 @@
 import { Router } from "express";
-import multer from "multer";
 import fileController from "../controllers/FileController";
 import logRequests from "../middlewares/logRequests";
 import loginRequired from "../middlewares/loginRequired";
-import multerConfig from "../config/multer";
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
 router.use(logRequests);
 
-router.post("/", upload.single("file"), fileController.store);
+router.post("/", fileController.store);
 
 export default router;
