92531da975f1f2c84185432e668d599969b4eb46
Leonardo Longobuco <longobucoleonardo@gmail.com>
2021-10-09 23:41:14 -0300

    correção pós deploy

--- server.js
+++
@@ -1,7 +0,0 @@
-import app from "./app";
-
-const port = 3001;
-app.listen(port, () => {
-  console.log(`Server up at port ${port} 🚀`);
-  console.log(`CTRL + Clique em: http://localhost:${port}`);
-});
