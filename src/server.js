e3e90327d4dea4225043e5db192098a522fa5025
Leonardo Longobuco <longobucoleonardo@gmail.com>
2021-10-10 19:20:18 -0300

    update de configs

--- src/server.js
+++ src/server.js
@@ -1,7 +1,4 @@
 import app from "./app";
 
-const port = 3001;
-app.listen(port, () => {
-  console.log(`Server up at port ${port} 🚀`);
-  console.log(`CTRL + Clique em: http://localhost:${port}`);
-});
+const port = process.env.APP_PORT;
+app.listen(port);
